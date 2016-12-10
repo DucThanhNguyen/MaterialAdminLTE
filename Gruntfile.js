e0552d6126888a48de0ab5d226b91e4c6914e4dd
DucThanhNguyen <ducthanh1504@gmail.com>
2016-12-10 21:09:30 +0700

    fix material icons url, change all skins file name

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function (grunt) {
           "dist/css/skins/skin-green-light.min.css": "build/less/skins/skin-green-light.less",
           "dist/css/skins/skin-red-light.min.css": "build/less/skins/skin-red-light.less",
           "dist/css/skins/skin-purple-light.min.css": "build/less/skins/skin-purple-light.less",
-          "dist/css/skins/_all-skins.min.css": "build/less/skins/_all-skins.less"
+          "dist/css/skins/all-skins.min.css": "build/less/skins/_all-skins.less"
         }
       }
     },
