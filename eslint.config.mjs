defd14c81941d38d88bbb7543f3700876711198e
Thanh Nguyen <ducthanh1504@gmail.com>
2024-04-29 11:25:33 +0700

    Add Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,8 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
