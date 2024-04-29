9b600d11961381ddbcd7a11ff00c7ca054f77ada
Thanh Nguyen <ducthanh1504@gmail.com>
2024-04-29 14:31:21 +0700

    Update app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Material Admin LTE/i);
   expect(linkElement).toBeInTheDocument();
 });
