a7d2636180dd56055f35d45e21de06348b6041c3
Thanh Nguyen <ducthanh1504@gmail.com>
2024-05-01 18:07:50 +0700

    Add page breadcrumbs

---
+++ src/components/layout/PageBreadcrumbs.tsx
@@ -0,0 +1,61 @@
+import { Breadcrumbs, Link, LinkProps, Typography } from "@mui/material";
+import { IconHome } from "@tabler/icons-react";
+import { FC, ReactNode } from "react";
+import { Link as RouterLink, useLocation } from "react-router-dom";
+
+interface LinkRouterProps extends LinkProps {
+  to: string;
+  replace?: boolean;
+}
+
+function LinkRouter(props: LinkRouterProps) {
+  return <Link {...props} component={RouterLink as any} />;
+}
+
+interface Props {
+  title?: string | ReactNode;
+}
+
+export const PageBreadcrumbs: FC<Props> = ({ title }) => {
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
+
+  const breadcrumbNameMap: { [key: string]: string } = {
+    "/layout": "Layout Options",
+    "/charts": "Charts",
+    "/ui": "UI Elements",
+    "/tables": "Tables",
+  };
+
+  return (
+    <Breadcrumbs separator="›" aria-label="breadcrumb">
+      <Link
+        underline="hover"
+        sx={{ display: "flex", alignItems: "center" }}
+        color="inherit"
+        href="/"
+      >
+        <IconHome size={18} strokeWidth={1.5} />
+      </Link>
+      {pathnames.length === 0 && (
+        <Typography color="text.secondary">{title}</Typography>
+      )}
+      {pathnames.map((value, index) => {
+        const last = index === pathnames.length - 1;
+        const to = `/${pathnames.slice(0, index + 1).join("/")}`;
+
+        return last ? (
+          <Typography color="text.secondary" key={to}>
+            {title}
+          </Typography>
+        ) : (
+          <LinkRouter underline="hover" color="inherit" to={to} key={to}>
+            <Typography color={(theme) => theme.palette.primary.main}>
+              {breadcrumbNameMap[to] || value}
+            </Typography>
+          </LinkRouter>
+        );
+      })}
+    </Breadcrumbs>
+  );
+};
--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,28 +1,48 @@
-import { Stack, StackProps, Typography } from "@mui/material";
+import {
+  Stack,
+  StackProps,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { FC, ReactNode } from "react";
+import { PageBreadcrumbs } from "./PageBreadcrumbs";
 
 interface Props extends Omit<StackProps, "title"> {
   title?: string | ReactNode;
+  breadcrumbTitle?: string | ReactNode;
   actions?: ReactNode;
 }
 
 export const PageLayout: FC<Props> = ({
   title,
+  breadcrumbTitle,
   actions,
   children,
   ...props
 }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Stack {...props}>
-      <Stack direction="row" alignItems="center" justifyContent="space-between">
+      <Stack
+        direction={isMobile && !actions ? "column" : "row"}
+        alignItems="center"
+        justifyContent="space-between"
+      >
         {typeof title === "string" ? (
-          <Typography component="h1" variant="h4" fontWeight={500}>
+          <Typography component="h1" fontSize="1.8rem">
             {title}
           </Typography>
         ) : (
           title
         )}
-        {actions}
+        {actions ? (
+          actions
+        ) : (
+          <PageBreadcrumbs title={breadcrumbTitle || title} />
+        )}
       </Stack>
       {children}
     </Stack>
